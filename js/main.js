86923fac9bb819516ed7d76ac85602c232d52e91
Mistry Pavan Kumar <mistry_19e11a0581@biet.ac.in>
2020-07-15 13:45:45 +0530

    logo animation

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function bigImg(x) {
+    x.style.height = "150px";
+    x.style.transitionDuration = "1s";
+    x.style.cursor = "pointer";
+}
+
+function normalImg(x) {
+    x.style.height = "100px";
+}
